6766095c89b42346de2ff10da02d8757c7b91aba
Artem <klaid-87@mail.ru>
2022-06-14 21:52:19 +0300

    corrected point 30

--- script.js
+++ script.js
@@ -56,9 +56,9 @@ let age_2 = 18
 let age_3 = 60
 //  29. Создать if в котором будите проверять значение переменной age_1
 if(age_1 < age_2) {
-  console.log("You don’t have access cause your age is " + age_1 + " It’s less then")
+  console.log("You don’t have access cause your age is " + age_1 + " It’s less then " + age_2)
 } else if(age_1 >= age_2 && age_1 < age_3) {
-  console.log("Welcome  !")
+  console.log("Welcome!")
 } else if(age_1 > age_3) {
   console.log("Keep calm and look Culture channel")
 } else {
